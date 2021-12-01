1e6a4181e7c81eaab8bbd892d5c8412a29db5b6b
siarhei <homerj112@yandex.ru>
2021-12-01 20:23:49 +0300

    added styles

--- HW_11/js/script.js
+++ HW_11/js/script.js
@@ -4,14 +4,14 @@ class User {
     constructor(object) {
         this.data = {
             id : object.id || 0,
-            name : object.name || "",
-            address : object.address || "",
-            email : object.email || "",
-            phone : object.phone || ""
+            name : object.name || "unknown",
+            address : object.address || "- -",
+            email : object.email || "- -",
+            phone : object.phone || "unknown"
         } 
     }
 
-    edit(obj){
+    edit(obj) {
         this.data.name = obj.name || this.data.name ;
         this.data.address = obj.address || this.data.address;
         this.data.email = obj.email || this.data.email;
@@ -27,18 +27,14 @@ class User {
 
 
 
-
 class Contacts {
     
     data = new Array();
-    constructor(){
-        
-    }
-    
+
     add(obj) {
           let user = new User(obj);
-          console.log(user.data.id, user, this.data)
-          this.data.push(user)
+          this.data.push(user.get());
+
     }
 
     edit(id, obj) {
@@ -49,14 +45,13 @@ class Contacts {
         })
 
         editUser.edit(obj);
-        
     }
 
     remove(id) {
         this.data.forEach(item => {
-            if(item.data.id == id){
+            if(item.data.id == id) {
                 const index = this.data.indexOf(item);
-                this.data.splice(index,1)
+                this.data.splice(index, 1);
             } 
         });
     }
@@ -70,160 +65,174 @@ class Contacts {
 
 class ContactsApp extends Contacts{
    
-     appEl = document.createElement('div');
+    appEl = document.createElement('div');
    
     constructor() {
-     super()
+        super();
+
         this.id = 1;
 
         this.appEl.classList.add('container')
         document.querySelector('body').append(this.appEl);
 
         this.appEl.innerHTML = `
-        <div class="show">
-            <div class="contacts"></div>
-        </div>
-
-        <div class="form hidden">
-            <form action="#" >
-            
-                <input type="text" class="name" name="name">
-                <input type="text" class="address" name="address">
-                <input type="email" class="email" name="email">
-                <input type="tel" class="phone" name = "phone">
-                <button class="submit added">submit</button>
-            </form>
-        </div>
-
         <div class="head__contacts">
             <h2>Contacts</h2>
             <div class="add"></div>
         </div>
+
+        <div class="form hidden">
+            <form action="#" method = "GET">
+           
+                <div class="field">Name<input  type="text" class="name" name="name"></div>
+                <div class="field">Phone<input type="tel" class="phone" name = "phone"></div>
+                <div class="field">Address<input type="text" class="address" name="address"></div>
+                <div class="field">Email<input type="email" class="email" name="email"></div>
+                <button class="submit added">submit</button>
+            </form>
+        </div>
+        <div class="contacts"></div>
+
+        
     `
     }
 
-    onAdd(){
-        let d = {};
+    onAdd() {
 
-        d.id = this.id;
-        d.name = document.querySelector('.name').value;
-        d.address = document.querySelector('.address').value;
-        d.email = document.querySelector('.email').value;
-        d.phone = document.querySelector('.phone').value;
+        let contact = {};
 
-        document.querySelector('.address').value = "";
-        document.querySelector('.email').value = "";
-        document.querySelector('.name').value = "";
-        document.querySelector('.phone').value = "";
+        contact.id = this.id;
+        contact.name = document.querySelector('.name').value;
+        contact.address = document.querySelector('.address').value;
+        contact.email = document.querySelector('.email').value;
+        contact.phone = document.querySelector('.phone').value;
 
-        super.add(d);
+        // document.querySelector('.address').value = "";
+        // document.querySelector('.email').value = "";
+        // document.querySelector('.name').value = "";
+        // document.querySelector('.phone').value = "";
+        console.log(contact)
+        
+
+        super.add(contact);
+        this.get();
         this.id++;
     }
 
-    onEdit(){
+    onEdit() {
         let contact = event.target.closest("div .contact__item");
-        let ad = {};
-        ad.name = document.querySelector('.name').value;
-        ad.address = document.querySelector('.address').value;
-        ad.email = document.querySelector('.email').value;
-        ad.phone = document.querySelector('.phone').value;
+        let form = document.querySelector('.edit__form')
+        let editedContact = {};
         let id = contact.dataset.id;
-        super.edit(id,ad);
+
+        editedContact.name = form.querySelector('.name').value;
+        editedContact.address = form.querySelector('.address').value;
+        editedContact.email = form.querySelector('.email').value;
+        editedContact.phone = form.querySelector('.phone').value;
+
+        super.edit(id, editedContact);
         this.get();
     }
 
-    onRemove(){
+    onRemove() {
         let contact = event.target.closest("div .contact__item");
         let id = contact.dataset.id;
+
         super.remove(id);
         this.get();
     }
 
-    get(){
-        let c = document.querySelector('.contacts');
-        let b = super.get();
+    showContacts(item) {
+        let contactItem = document.createElement('div');
+        contactItem.setAttribute('data-id', item.data.id);
+        contactItem.classList.add('contact__item');
 
-        while(c.firstChild) {
-            c.removeChild(c.firstChild);
+        let main  = document.createElement('div');
+        main.classList.add('contact__item__main');
+
+        let id  = document.createElement('div');
+        id.classList.add('contact__item__id');
+
+        let content = document.createElement('div');
+        content.classList.add('contact__item__content');
+
+        let buttons = document.createElement('div');
+        buttons.classList.add('contact__item__buttons');
+        
+        let editButton = document.createElement('div');
+        editButton.classList.add('edit');
+
+        let deleteButton = document.createElement('div');
+        deleteButton.classList.add('delete');
+        
+        main.append(content)
+        buttons.append(editButton,deleteButton);
+        contactItem.append(main,buttons);
+
+        id.innerHTML = `${item.data.id}`;
+        content.innerHTML = ` <h2> Name: <span>${item.data.name}</span> </br>Phone: <span>${item.data.phone}</span> </h2> email: <span>${item.data.email}</span>;   address: <span>${item.data.address}</span> </br>`;
+        
+        document.querySelector('.contacts').prepend(contactItem);
+
+        editButton.addEventListener('click', (event) => {
+            content.innerHTML = `
+            <div class="form edit__form">
+                <form action="#" >
+                    <input type="text" class="name" name="name" placeholder="NAME: ${item.data.name}">
+                    <input type="tel" class="phone" name = "phone" placeholder="PHONE: ${item.data.phone}">
+                    <input type="text" class="address" name="address" placeholder="ADDRESS: ${item.data.address}">
+                    <input type="email" class="email" name="email" placeholder="EMAIL: ${item.data.email}">
+                   
+                    <button class="submit edited">edit contact</button>
+                </form>
+            </div>`;
+            editButton.addEventListener('click',() => this.get());
+
+            let editConfirmButt = document.querySelector('.edited');
+
+            editConfirmButt.addEventListener('click', (event) => {
+                this.onEdit();
+                this.get();
+            }); 
+        });
+
+
+        deleteButton.addEventListener('click', (event) => {
+            this.onRemove();    
+        });
+    }
+
+    get() {
+        let contatsData = super.get();
+        let contacts = document.querySelector('.contacts');
+        
+        while(contacts.firstChild) {
+            contacts.removeChild(contacts.firstChild);
         }
 
-        b.forEach(item => {
-            let p = document.createElement('div');
-            p.setAttribute('data-id',item.data.id);
-            p.classList.add('contact__item');
-            let id  = document.createElement('div');
-            id.classList.add('contact__item__id');
-            let content = document.createElement('div');
-            content.classList.add('contact__item__content');
-            let buttons = document.createElement('div');
-            buttons.classList.add('contact__item__buttons');
-            let edit = document.createElement('div');
-            let del = document.createElement('div');
-            edit.classList.add('edit');
-            del.classList.add('delete');
-            buttons.append(edit,del);
-            p.append(id,content,buttons);
-            id.innerHTML = `${item.data.id}`;
-            content.innerHTML = ` <h2> Name: ${item.data.name} </br>Phone: ${item.data.phone} </h2> email: ${item.data.email} address: ${item.data.address} </br>`;
-            
-            c.append(p);
-
-            edit.addEventListener('click', (event) => {
-                content.innerHTML += `
-                <div class="form edit">
-            
-                    <form action="#" >
-                    
-                        <input type="text" class="name" name="name">
-                        <input type="text" class="address" name="address">
-                        <input type="email" class="email" name="email">
-                        <input type="tel" class="phone" name = "phone">
-                        <button class="submit edited">edit contact</button>
-                    </form>
-                </div>`
-
-                
-                let conta = document.querySelector('.edited');
-
-                conta.addEventListener('click', (event) => {
-                           this.onEdit();
-                           this.get();
-                        }); 
-            });
-
-            del.addEventListener('click', (event) => {
-            this.onRemove();    
-            });
-
-        })
+        contatsData.forEach(item =>  this.showContacts(item));
     }
     
     init() {
         
-        let but = document.querySelector('.added');
-        let add = document.querySelector('.add');
-        
-        add.addEventListener('click', ()=>{
-            document.querySelector(".form").classList.remove('hidden')
-        })
-        
-        
-        but.addEventListener('click', () => {
-
-            if(but.classList.contains('added')){
-                this.onAdd();
-                this.get()
-                document.querySelector(".form").classList.add('hidden')
-            }
+        let submitAddCont = document.querySelector('.added');
+        let addContact = document.querySelector('.add');
+        let form = document.querySelector(".form")
+       
+        addContact.addEventListener('click', () => form.classList.toggle('hidden'));
             
-        })
-        
+        submitAddCont.addEventListener('click', () => {
+            
+            this.onAdd();
+            this.get();
+            form.classList.add('hidden');
+        });
     }
 }
 
 
-let app = new ContactsApp();
-app.init();
+let application = new ContactsApp();
+application.init();
 
 
 // let a = {
